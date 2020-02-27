e54e55186c7a1c3c40c3222d5723fb609f64b1a9
Matthew Merks <matthew@pbos.co.za>
2020-02-27 18:24:30 +0200

    get and add transactions

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,6 +1,44 @@
-// @desc    GET all transactions
+const Transaction = require('../models/Transaction');
+
+// @desc    Get all transactions
 // @route   GET /api/v1/transactions
 // @access  public
-exports.getTransactions = (req, res, next) => {
-	res.send('GET transactions');
+exports.getTransactions = async (req, res, next) => {
+	try {
+		const transactions = await Transaction.find();
+
+		return res.status(200).json({
+			success: true,
+			count: transactions.length,
+			data: transactions
+		});
+	} catch (err) {
+		return res.send(500).json({
+			success: false,
+			error: 'Server Error'
+		});
+	}
+};
+
+// @desc    Add transaction
+// @route   POST /api/v1/transactions
+// @access  public
+exports.addTransaction = async (req, res, next) => {
+	try {
+		const { text, amount } = req.body;
+
+		const transaction = await Transaction.create(req.body);
+
+		return res.send(201).json({
+			success: true,
+			data: transaction
+		});
+	} catch (err) {}
+};
+
+// @desc    Delete transaction
+// @route   DELETE /api/v1/transactions/:id
+// @access  public
+exports.deleteTransaction = async (req, res, next) => {
+	res.send('DELETE transaction');
 };
--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
+
+connectDB();
 
 const transactions = require('./routes/transactions');
 
 const app = express();
+
+app.use(express.json());
 
 app.use('/api/v1/transactions', transactions);
 
@@ -16,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(
 	PORT,
 	console.log(
-		`Server running in ${process.env.NODE_ENV} on port ${PORT}`.bgBlue.yellow
+		`Server running in ${process.env.NODE_ENV} on port ${PORT}`.bgGreen.black
 			.bold
 	)
 );
